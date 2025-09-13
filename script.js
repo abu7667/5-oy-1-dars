b7c694ec2b0efebb19df9ee2b201e4a385f2f6d3
abu7667 <fijiim3@gmail.com>
2025-09-13 15:52:53 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let ism = prompt('Salom, ismingizni kiriting:');
+let familiya = prompt('Familiyangizni kiriting:');
+let yosh = prompt('Yoshingizni kiriting:');
+let fan = prompt('Yoqtirgan faningizni kiriting:');
+let manzil = prompt('Turar joyingizni kiriting:');
+
+let javob = 
+  'Sizning ismingiz: ' + ism + '\n' +
+  'Familiyangiz: ' + familiya + '\n' +
+  'Yoshingiz: ' + yosh + '\n' +
+  'Yoqtirgan faningiz: ' + fan + '\n' +
+  'Manzilingiz: ' + manzil;
+
+console.log(javob);
